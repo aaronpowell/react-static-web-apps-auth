6fe7d0037442e8968b5ec7e0adbddf663a05895a
Aaron Powell <me@aaron-powell.com>
2022-05-16 16:04:48 +1000

    updating pipelines for workspaces

--- react-static-web-apps-auth/.eslintrc.js
+++ react-static-web-apps-auth/.eslintrc.js
@@ -1,23 +1,22 @@
 module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:react/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: "latest",
     sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
+  plugins: ["@typescript-eslint"],
+
   settings: {
     react: {
       version: "detect",
     },
   },
-  extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-  ],
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": "off",
   },

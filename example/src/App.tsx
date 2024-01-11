feeba296921058a43bd13cb6b4b8d2c60add377e
Aaron Powell <me@aaron-powell.com>
2024-01-11 23:57:29 +0000

    Updating all deps and moving from CRA to vite for example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import logo from "./logo.svg";
+import logo from "./assets/react.svg";
 import "./App.css";
 import {
   Logout,
@@ -41,17 +40,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <StaticWebAuthLogins
           customProviders={[{ id: "okta", name: "Okta" }]}
           label={(name) => `Do sign in ${name}`}
